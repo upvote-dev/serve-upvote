a98e12e7c55ba9d83d8e5efca4d0f9d2e622cc9a
Samuel Marks <807580+SamuelMarks@users.noreply.github.com>
2024-11-15 21:07:07 -0600

    [src/main.rs] Add OpenAPI support and some nice HTML rendered outputs for these generated docs

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,14 @@
+use utoipa::{Modify, OpenApi};
+use utoipa_actix_web::AppExt;
+use utoipa_redoc::Servable;
+use utoipa_scalar::Servable as ScalarServable;
+
 #[cfg(test)]
 mod tests;
 
-pub const CARGO_PKG_VERSION: &'static str = env!("CARGO_PKG_VERSION");
+pub const CARGO_PKG_DESCRIPTION: &'static str = env!("CARGO_PKG_DESCRIPTION");
 pub const CARGO_PKG_NAME: &'static str = env!("CARGO_PKG_NAME");
+pub const CARGO_PKG_VERSION: &'static str = env!("CARGO_PKG_VERSION");
 
 #[derive(clap::Parser)]
 #[command(version, about, long_about = None)]
@@ -28,7 +34,7 @@ struct Cli {
     env: Option<Vec<String>>,
 }
 
-#[derive(serde::Deserialize, serde::Serialize, Debug, PartialEq)]
+#[derive(serde::Deserialize, serde::Serialize, utoipa::ToSchema, Debug, PartialEq)]
 struct Version {
     version: &'static str,
     upvote_backend: &'static str,
@@ -93,8 +99,40 @@ async fn main() -> std::io::Result<()> {
     let manager = diesel::r2d2::ConnectionManager::<diesel::PgConnection>::new(database_url);
     let pool = diesel::r2d2::Pool::builder().build(manager).unwrap();
 
+    #[derive(utoipa::OpenApi)]
+    #[openapi(
+        tags(
+            (name = CARGO_PKG_NAME, description = CARGO_PKG_DESCRIPTION)
+        )
+    )]
+    struct ApiDoc;
+
+    struct SecurityAddon;
+
+    impl Modify for SecurityAddon {
+        fn modify(&self, openapi: &mut utoipa::openapi::OpenApi) {
+            let components = openapi.components.as_mut().unwrap(); // we can unwrap safely since there already is components registered.
+            components.add_security_scheme(
+                "password",
+                utoipa::openapi::security::SecurityScheme::OAuth2(
+                    utoipa::openapi::security::OAuth2::new([
+                        utoipa::openapi::security::Flow::Password(
+                            utoipa::openapi::security::Password::new(
+                                "/api/token",
+                                utoipa::openapi::security::Scopes::new(),
+                            ),
+                        ),
+                    ]),
+                ),
+            )
+        }
+    }
+
     actix_web::HttpServer::new(move || {
         actix_web::App::new()
+            .into_utoipa_app()
+            .openapi(ApiDoc::openapi())
+            .map(|app| app.wrap(actix_web::middleware::Logger::default()))
             .app_data(
                 actix_web::web::JsonConfig::default().error_handler(|err, _req| {
                     actix_web::error::InternalError::from_response(
@@ -108,11 +146,11 @@ async fn main() -> std::io::Result<()> {
             )
             .app_data(actix_web::web::Data::new(pool.clone()))
             .service(
-                actix_web::web::scope("/api/v0_noauth")
+                utoipa_actix_web::scope("/api/v0_noauth")
                     .service(upvote_backend::routes::review::read_many),
             )
             .service(
-                actix_web::web::scope("/api/v0")
+                utoipa_actix_web::scope("/api/v0")
                     .wrap(actix_web::middleware::Compat::new(
                         actix_web_httpauth::middleware::HttpAuthentication::bearer(
                             rust_actix_diesel_auth_scaffold::middleware::bearer::validator,
@@ -124,13 +162,13 @@ async fn main() -> std::io::Result<()> {
                     .service(upvote_backend::routes::review::read),
             )
             .service(
-                actix_web::web::scope("/api")
-                    .service(rust_actix_diesel_auth_scaffold::routes::token::token)
-                    .service(rust_actix_diesel_auth_scaffold::routes::authorisation::authorise)
-                    .service(version),
+                utoipa_actix_web::scope("/api")
+                    .service(rust_actix_diesel_auth_scaffold::routes::token::token),
+                // .service(rust_actix_diesel_auth_scaffold::routes::authorisation::authorise)
+                // .service(version),
             )
             .service(
-                actix_web::web::scope("/secured")
+                utoipa_actix_web::scope("/secured")
                     .wrap(actix_web::middleware::Compat::new(
                         actix_web_httpauth::middleware::HttpAuthentication::bearer(
                             rust_actix_diesel_auth_scaffold::middleware::bearer::validator,
@@ -139,6 +177,16 @@ async fn main() -> std::io::Result<()> {
                     .service(rust_actix_diesel_auth_scaffold::routes::secret::secret)
                     .service(rust_actix_diesel_auth_scaffold::routes::logout::logout),
             )
+            .openapi_service(|api| utoipa_redoc::Redoc::with_url("/redoc", api))
+            .openapi_service(|api| {
+                utoipa_swagger_ui::SwaggerUi::new("/swagger-ui/{_:.*}")
+                    .url("/api-docs/openapi.json", api)
+            })
+            .map(|app| {
+                app.service(utoipa_rapidoc::RapiDoc::new("/api-docs/openapi.json").path("/rapidoc"))
+            })
+            .openapi_service(|api| utoipa_scalar::Scalar::with_url("/scalar", api))
+            .into_app()
     })
     .bind((args.hostname.as_str(), args.port))?
     .run()
